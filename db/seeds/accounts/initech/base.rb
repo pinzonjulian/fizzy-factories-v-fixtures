5c198f4b744285bf1b39f38e92a7b57722818273
Julián Pinzón Eslava <julian@julianpinzon.com>
2025-12-20 22:44:22 +1100

    use oaken accessors instead of local variables

--- db/seeds/accounts/initech/base.rb
+++ db/seeds/accounts/initech/base.rb
@@ -1,35 +1,34 @@
 # Initech account scenario
 
 # Create the account (entropy and join_code are auto-created via callbacks)
-account = accounts.create :initech, name: "Initech LLC"
-Current.account = account
+accounts.create :initech, name: "Initech LLC"
+Current.account = accounts.initech
 
 # Label the auto-created entropy and join_code for test access
-entropies.label initech_account: account.entropy
-account_join_codes.label initech: account.join_code
+entropies.label initech_account: accounts.initech.entropy
+account_join_codes.label initech: accounts.initech.join_code
 
 # Update the join code to match fixtures
-account.join_code.update!(code: "INIT-5678-9XYZ", usage_count: 10, usage_limit: 10)
+accounts.initech.join_code.update!(code: "INIT-5678-9XYZ", usage_count: 10, usage_limit: 10)
 
 # User for Initech (mike_identity was created in 37s.rb since he has a session there)
-mike_identity = identities.mike
-mike = users.create :mike, name: "Mike", role: :admin, identity: mike_identity, account: account, verified_at: Time.current
-system_user = users.create :system_initech, name: "System", role: :system, account: account
+users.create :mike, name: "Mike", role: :admin, identity: identities.mike, account: accounts.initech, verified_at: Time.current
+users.create :system_initech, name: "System", role: :system, account: accounts.initech
 
 # Set current user for callbacks
-Current.user = mike
+Current.user = users.mike
 
 # Milton's Wish List board
-miltons_wish_list = boards.create :miltons_wish_list, name: "Milton's Wish List", creator: mike, all_access: true, account: account
+boards.create :miltons_wish_list, name: "Milton's Wish List", creator: users.mike, all_access: true, account: accounts.initech
 
 # Entropy for board (board entropy is optional)
-entropies.create :miltons_wish_list_board, account: account, container: miltons_wish_list, auto_postpone_period: 90.days.to_i
+entropies.create :miltons_wish_list_board, account: accounts.initech, container: boards.miltons_wish_list, auto_postpone_period: 90.days.to_i
 
 # Cards
-radio = cards.create :radio, account: account, board: miltons_wish_list, creator: mike,
+cards.create :radio, account: accounts.initech, board: boards.miltons_wish_list, creator: users.mike,
   number: 1, title: "I want to play my radio at a reasonable volume",
   created_at: 1.week.ago, status: :published, last_active_at: 1.week.ago
 
-paycheck = cards.create :paycheck, account: account, board: miltons_wish_list, creator: mike,
+cards.create :paycheck, account: accounts.initech, board: boards.miltons_wish_list, creator: users.mike,
   number: 2, title: "I haven't received my paycheck",
   created_at: 1.week.ago, status: :published, last_active_at: 1.week.ago
